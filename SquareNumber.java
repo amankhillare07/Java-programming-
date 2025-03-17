ed7e8bbbe092ae3c272506ccf18c503b73ca6658
amankhillare07 <amankhillare097@gmail.com>
2025-03-17 22:10:48 +0530

    Create SquareNumber.java

---
+++ SquareNumber.java
@@ -0,0 +1,8 @@
+ class SquareNumber {  
+    public static void main(String[] args) {  
+        int num = 4;  
+        int square = num * num;  
+
+        System.out.println("Square of " + num + " is: " + square);  
+    }  
+}
