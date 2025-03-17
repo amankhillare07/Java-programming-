7611ed2b3ffb5d79906092fab3bb207f3168c003
amankhillare07 <amankhillare097@gmail.com>
2025-03-17 22:19:04 +0530

    Create HierarchicalExample.java

---
+++ HierarchicalExample.java
@@ -0,0 +1,29 @@
+
+class Vehicle {  
+    void start() {  
+        System.out.println("Vehicle is starting...");  
+    }  
+}  
+class Car extends Vehicle {  
+    void drive() {  
+        System.out.println("Car is driving");  
+    }  
+}  
+class Bike extends Vehicle {  
+    void ride() {  
+        System.out.println("Bike is being ridden");  
+    }  
+}  
+public class HierarchicalExample {  
+    public static void main(String[] args) {  
+        Car myCar = new Car();  
+        myCar.start();   
+        myCar.drive();   
+
+        System.out.println();  
+
+        Bike myBike = new Bike();  
+        myBike.start();   
+        myBike.ride();     
+    }  
+}
